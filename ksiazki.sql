833fd1ec06ad46d83c35e1ff392a5428592ddf12
Franek101 <108134719+Franek101@users.noreply.github.com>
2022-11-23 14:52:33 +0100

    Add files via upload

---
+++ ksiazki.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 23 Lis 2022, 14:49
+-- Wersja serwera: 10.4.25-MariaDB
+-- Wersja PHP: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `biblioteka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ksiazki`
+--
+
+CREATE TABLE `ksiazki` (
+  `id_ksiazki` int(11) NOT NULL,
+  `tytuł` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `autor` varchar(100) COLLATE utf8mb4_polish_ci NOT NULL,
+  `wydawnictwo` varchar(70) COLLATE utf8mb4_polish_ci NOT NULL,
+  `rok_wydania` int(11) NOT NULL,
+  `id_kategorii` int(11) NOT NULL,
+  `opis` text COLLATE utf8mb4_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  ADD PRIMARY KEY (`id_ksiazki`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  MODIFY `id_ksiazki` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
